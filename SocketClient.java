36739ce6be9c5d5062b80a7f9e9197396865644f
JAgrit20 <jagritacharya2020@gmail.com>
2021-09-16 11:38:49 +0530

    first commit

---
+++ SocketClient.java
@@ -0,0 +1,59 @@
+import java.net.*;
+
+import java.io.*;
+
+public class SocketClient
+
+{
+
+public static void main(String args[])
+
+{
+
+Socket server;
+
+int port=10000;
+
+DataInputStream dis;
+
+PrintStream ps;
+
+String host="localhost";
+
+System.out.println("Starting Client Application");
+
+System.out.println("Trying to open the connection");
+
+try
+
+{
+
+server=new Socket(host,port);
+
+System.out.println("Successfully opened ServerSocket");
+
+dis=new DataInputStream(server.getInputStream());
+
+ps=new PrintStream(server.getOutputStream());
+
+ps.println("Hello");
+
+String s=dis.readLine();
+
+System.out.println("The server:"+s);
+
+ps.println("Hello");
+
+}
+
+catch(Exception e)
+
+{
+
+System.out.println(e);
+
+}
+
+}
+
+}
---
+++ SocketServer.java
@@ -0,0 +1,67 @@
+import java.net.*;
+
+import java.io.*;
+
+public class SocketServer
+
+{
+
+public static void main(String args[])
+
+{
+
+ServerSocket server;
+
+Socket client;
+
+int port=10000;
+
+DataInputStream dis;
+
+PrintStream ps;
+
+System.out.println("Starting Server Application");
+
+System.out.println("Trying to open the connection");
+
+try
+
+{
+
+server=new ServerSocket(port);
+
+System.out.println("Successfully opened ServerSocket");
+
+System.out.println("waiting for client connection");
+
+client=server.accept();
+
+System.out.println("Client connection established");
+
+System.out.println("IP address of client is "+client.getInetAddress());
+
+dis=new DataInputStream(client.getInputStream());
+
+ps=new PrintStream(client.getOutputStream());
+
+String s=dis.readLine();
+
+System.out.println("The client:"+s);
+
+ps.println("Hello");
+
+}
+
+catch(Exception e)
+
+{
+
+System.out.println(e);
+
+}
+
+}
+
+}class sever {
+    
+}
